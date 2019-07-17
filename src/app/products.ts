f83bc7db2238a350264e7f3a88c6a6537dbb0fdb
pavanpej <pavanpej@gmail.com>
2019-07-17 17:54:08 +0530

    add product list

---
+++ src/app/products.ts
@@ -0,0 +1,21 @@
+export const products = [
+  {
+    name: "JCB Crane",
+    image: "https://4.imimg.com/data4/BB/BB/GLADMIN-/images-imm3-250x250.jpg",
+    description:
+      "A crane capable of lifting up to 40 metric tonnes. Can be operated by a single person. Refuelled and ready to go."
+  },
+  {
+    name: "Caterpillar Excavator",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81U-DMn3SvL._SX679_.jpg",
+    description:
+      "An earth mover with a capacity of removing up to 200kg of earth material. Needs 2 people to operate. Caution! Heavy machinery."
+  },
+  {
+    name: "Lonking Road Roller",
+    image: "http://img.diytrade.com/cdimg/1496079/22301761/0/1310002720.jpg",
+    description:
+      "Heavy road roller to be used only on roads with a minimum of 2 lanes. Has 1 main heavy duty metal roller, and 2 rear steered tyres."
+  }
+];
