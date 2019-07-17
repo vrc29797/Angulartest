cfc5d081f9c23c081de629f831847e2e149a786c
pavanpej <pavanpej@gmail.com>
2019-07-17 17:59:18 +0530

    add product details page

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { DataService } from "../data.service";
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
 })
 export class ProductComponent implements OnInit {
+  product;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get("productId")];
+    });
   }
-
 }
